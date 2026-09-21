ace66358eb50dd7b52cbc26e76c15c5301b91950
popkid <premiumpopkid@gmail.com>
2026-09-21 23:38:16 +0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -13,7 +13,14 @@ cmd({
     const latency = Date.now() - start;
 
     await sock.sendMessage(m.from, {
-        text: `🏓 Pong!\nLatency: ${latency}ms`,
+        text: `┏▣ ◈ *𝗣𝗢𝗣𝗞𝗜𝗗 𝗣𝗜𝗡𝗚* ◈
+┃
+┃🏓 *𝗣𝗢𝗡𝗚!* 
+┃
+┃➽ *𝗟𝗔𝗧𝗘𝗡𝗖𝗬* : ${latency}ms
+┃➽ *𝗦𝗧𝗔𝗧𝗨𝗦* : 𝗢𝗡𝗟𝗜𝗡𝗘
+┃
+┗▣`,
         edit: sent.key
     });
 });
